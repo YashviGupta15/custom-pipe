ba51cfcad8e5aeec164028c6c2b0adc795a8c29c
YashviGupta15 <yashvigupta12@gmail.com>
2022-01-08 18:14:25 +0530

    3 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { SearchPipe } from './search.pipe';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, SearchPipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
